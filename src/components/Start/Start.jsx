c79f9c9c19410b8f74e3cac8bea16659e5bf3896
diegonacif <diegoreisnacif@gmail.com>
2022-10-18 19:51:59 -0300

    version 1 done

--- src/components/Start/Start.jsx
+++ src/components/Start/Start.jsx
@@ -3,7 +3,7 @@ import momHeart from '../../assets/mother.png';
 export const Start = () => {
   return (
     <div className="start">
-      <p>Essa é uma pequena homenagem pra pessoa que sempre esteve presente nas nossas vidas dando o seu melhor. A pessoa que faz jus ao significado da palavra mãe. Àquela que nunca nos abandonou e que fez do propósito da própria vida, cuidar.<br /><br />Neste presente estão algumas imagens pra relembrar nossos momentos. Um Feliz aniversário, e que Deus possa nos conceder muitos anos juntos, onde alcançaremos horizontes maiores e melhores.</p>
+      <p>Essa é uma pequena homenagem pra pessoa que sempre esteve presente nas nossas vidas dando o seu melhor. A pessoa que faz jus ao significado da palavra mãe. Àquela que nunca nos abandonou e que fez do propósito da própria vida, cuidar.<br /><br />Neste presente estão algumas imagens pra relembrar momentos que guardamos nos corações. Um Feliz aniversário, e que Deus possa nos conceder muitos anos juntos, onde alcançaremos horizontes maiores e melhores.</p>
       <img src={momHeart} alt="Mom Heart"></img>
     </div>
   )
